1cc5c7f9f707661bb599d7c8469669070ceaec59
coderQls <1064341174@qq.com>
2022-12-28 10:25:15 +0800

    解析body

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,11 @@
 const path = require('path')
 const Koa = require('koa')
-
+const { koaBody } = require('koa-body')
 const userRouter = require(path.resolve(__dirname, '../router/user.route'))
 
 const app = new Koa()
 
+app.use(koaBody())
 app.use(userRouter.routes())
 
 module.exports = app
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,20 @@
+const path = require('path')
+const { createUser } = require(path.resolve(
+  __dirname,
+  '../service/user.service.js'
+))
+
 class UserController {
   async register(ctx, next) {
-    ctx.body = '用户注册成功'
+    // 1. 获取数据
+    const { user_name, password } = ctx.request.body
+
+    // 2. 操作数据库
+    const res = await createUser(user_name, password)
+    console.log(res)
+
+    // 3. 返回结果
+    ctx.body = ctx.request.body
   }
 
   async login(ctx, next) {
